60afde8eaf14049c3141009c410b9c5f9677227e
jiaoqiyuan <jiaoqiyuan@outlook.com>
2017-10-26 19:08:10 +0000

    first add chapter3

---
+++ chapter3-list/names-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+names = ["chen", "jim", "bob", "mike"]
+
+for name in names:
+    message = "I would like to own a " + name + " motorcycle"
+    print(message)
+
+#print(message + names[0])
+#print(message + names[1])
+#print(message + names[2])
+#print(message + names[3])
---
+++ chapter3-list/python-list.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = "My first bucycle was a " + bicycles[0].title() + "."
+print(message)

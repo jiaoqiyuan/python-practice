b1072c728ad5c4af0657e85d4fed446a908e4968
jiaoqiyuan <jiaoqiyuan@outlook.com>
2017-10-26 19:17:48 +0000

    add list(del insert)

---
+++ chapter3-list/motorcycles.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+del motorcycles[0]
+print(motorcycles)
+
+del motorcycles[1]
+print(motorcycles)

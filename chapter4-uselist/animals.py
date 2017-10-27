1585c1db8896fa0949817186c15b240fa4e3bfcd
jiaoqiyuan <jaioqiyuan@outlook.com>
2017-10-27 19:00:35 +0800

    add chapter4.1 4.2

---
+++ chapter4-uselist/animals.py
@@ -0,0 +1,5 @@
+animals = ['pig', 'chiken', 'tigger', 'bird', 'bull']
+for animal in animals:
+	print("A dog would make a great pet " + animal)
+
+print("Any of these animals would make a great pet!")
---
+++ chapter4-uselist/pisas.py
@@ -0,0 +1,5 @@
+pisas = ['New-York-Style', 'Chicago-Style', 'California-Style', 'Pan-Pizza']
+for pisa in pisas:
+	print("I like pepperoni pizza " + pisa)
+
+print("I like pizza very much, I really love pizza!")

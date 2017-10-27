754463e26ccdf4f59268b7767c8dfeaad8a60ced
jiaoqiyuan <jiaoqiyuan@outlook.com>
2017-10-27 22:43:26 +0800

    add some code

---
+++ buffet.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+buffets = ('chiken', 'milk', 'ege', 'soup')
+for buffet in buffets:
+    print(buffet)
+
+#buffets[1] = 'aaa'
+print("Now change the list!")
+buffets = ('chiken', 'milk_bak', 'ege_bak', 'soup')
+for buffet in buffets:
+    print(buffet)

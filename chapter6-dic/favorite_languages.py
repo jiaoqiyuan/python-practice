17ccf5ddae86038c6711cec6bde999bef2809ba4
jiaoqiyuan <jiaoqiyuan@outlook.com>
2017-11-01 23:05:32 +0800

    add new code

--- chapter6-dic/favorite_languages.py
+++ chapter6-dic/favorite_languages.py
@@ -70,3 +70,14 @@ for name in favorite_languages.keys():
 		print(name.title() + ", Thanks for your investegating!")
 	else:
 		print("Please accept my investigating!")
+
+favorite_languages = {
+	'jen': ['Python', 'ruby'],
+	'sarah': ['c'],
+	'edward': ['ruby', 'go'],
+	'phil': ['python', 'haskell'],
+}
+for name, languages in favorite_languages.items():
+	print("\n" + name.title() + "'s favorite languages are:")
+	for language in languages:
+		print("\t" + language.title())
--- chapter6-dic/user.py
+++ chapter6-dic/user.py
@@ -3,6 +3,24 @@ user_0 = {
 	'first': 'enrico',
 	'last': 'fermi',
 }
+user_1 = {
+        'username': 'jiao',
+        'first': 'jiao',
+        'last': 'qiyuan',
+}
+user_2 = {
+        'username': 'abcd',
+        'first': 'efg',
+        'last': 'higk',
+}
+
+
 for key, value in user_0.items():
 	print("\nKey: " + key)
 	print("Value: " + value)
+people = [user_0, user_1, user_2]
+
+for person in people:
+        for key, value in person.items():
+                print("\nKey: " + key + "\tvalue: " + value.title())
+

11997ccbe23512e50bf3f40af2814dced3fb7f91
jiaoqiyuan <jiaoqiyuan@outlook.com>
2017-11-03 20:13:35 +0800

    add chapter6

---
+++ chapter8-func/pets.py
@@ -0,0 +1,6 @@
+def descrive_pet(animal_type, pet_name):
+    """显示宠物信息"""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+descrive_pet('hamster', 'harry')

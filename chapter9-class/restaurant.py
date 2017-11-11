d21e9b562400fb38d870fbb3b9724df00cf3ab4f
jiaoqiyuan <jiaoqiyuan@outlook.com>
2017-11-11 14:51:49 +0800

    finish 9.1

--- chapter9-class/restaurant.py
+++ chapter9-class/restaurant.py
@@ -2,6 +2,7 @@ class Restaurant():
     def __init__(self, restaurant_name, cuisin_type):
         self.restaurant_name = restaurant_name
         self.cuisin_type = cuisin_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print("The restaurant's name is: " + self.restaurant_name)
@@ -10,12 +11,22 @@ class Restaurant():
     def open_restaurant(self):
         print("The restaurant is open.")
 
+    def set_number_served(self, number_served):
+    	self.number_served = number_served
+
+    def increment_number_served(self, number):
+    	self.number_served += number
+
 restaurant = Restaurant('qilixiang', 'zhongcan')
-restaurant1 = Restaurant('chouguiyu', 'huicai')
-restaurant2 = Restaurant('dayu', 'tiebanshao')
-print(restaurant.restaurant_name)
-print(restaurant.cuisin_type)
-restaurant.describe_restaurant()
-restaurant1.describe_restaurant()
-restaurant2.describe_restaurant()
-restaurant.open_restaurant()
+#restaurant1 = Restaurant('chouguiyu', 'huicai')
+#restaurant2 = Restaurant('dayu', 'tiebanshao')
+#print(restaurant.cuisin_type)
+#restaurant.describe_restaurant()
+#restaurant1.describe_restaurant()
+#restaurant2.describe_restaurant()
+#restaurant.open_restaurant()
+
+#restaurant.number_served = 150
+restaurant.set_number_served(180)
+restaurant.increment_number_served(100)
+print("There are " + str(restaurant.number_served) + " people had come.")
--- chapter9-class/user.py
+++ chapter9-class/user.py
@@ -2,6 +2,7 @@ class User():
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+        self.login_attempts = 0
 
     def describe_user(self):
         print("The user's full name is: " + self.first_name.title() + " " + self.last_name.title())
@@ -9,12 +10,25 @@ class User():
     def greet_user(self):
         print("Welcome, " + self.first_name.title() + " " + self.last_name.title())
 
-user1 = User('jiao', 'qiyuan')
-user2 = User('liu', 'cuicui')
-user3 = User('jiao', 'xuhan')
-user1.describe_user()
-user1.greet_user()
-user2.describe_user()
-user2.greet_user()
-user3.describe_user()
-user3.greet_user()
+    def increment_login_attempts(self):
+    	self.login_attempts += 1
+
+    def reset_login_attempts(self):
+    	self.login_attempts = 0
+
+#user1 = User('jiao', 'qiyuan')
+#user2 = User('liu', 'cuicui')
+#user3 = User('jiao', 'xuhan')
+#user1.describe_user()
+#user1.greet_user()
+#user2.describe_user()
+#user2.greet_user()
+#user3.describe_user()
+#user3.greet_user()
+
+user4 = User('jiao', 'qiyuan')
+user4.increment_login_attempts()
+user4.increment_login_attempts()
+print(user4.login_attempts)
+user4.reset_login_attempts()
+print(user4.login_attempts)

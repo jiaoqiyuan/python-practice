8ca05232fd5669891a8ac8a44decd30079321501
jiaoqiyuan <jiaoqiyuan@outlook.com>
2017-11-24 23:23:13 +0800

    add random points drew

--- visible_date/scatter_squares.py
+++ visible_date/scatter_squares.py
@@ -14,5 +14,5 @@ plt.tick_params(axis='both', which='major', labelsize=14)
 
 # 设置每个坐标轴的取值范围
 plt.axis([0, 1100, 0, 1100000])
-# plt.show()
-plt.savefig('squares_plot.png')
+plt.show()
+#plt.savefig('squares_plot.png')

a3e9743c6956529e632b2441b8afde9646967423
jiaoqiyuan <jiaoqiyuan@outlook.com>
2017-11-26 00:50:03 +0800

    add new visual code

--- visible_date/dice_visual.py
+++ visible_date/dice_visual.py
@@ -7,7 +7,7 @@ die_2 = Die(10)
 
 # 投几次筛子，并将结果存储在一个列表中
 results = []
-for roll_num in range(100):
+for roll_num in range(5000):
 #    print(die_1.roll())
 #    print(die_2.roll())
     result = die_1.roll() + die_2.roll()
@@ -16,14 +16,14 @@ for roll_num in range(100):
 # 分析结果
 frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
-print(max_result)
+#print(max_result)
 for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # 对结果进行可视化
 hist = pygal.Bar()
-hist.title = "Result of rolling one D6 1000 times."
+hist.title = "Result of rolling one D6 5000 times."
 hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
@@ -31,5 +31,5 @@ hist.y_title = "Frequency of Result"
 hist.add('D6 + D10', frequencies)
 hist.render_to_file('dice_visual.svg')
 
-print(results)
+#print(results)
 #print(frequencies)
